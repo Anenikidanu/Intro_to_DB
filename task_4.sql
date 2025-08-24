8a52f761de758395530b453788b99d50a65ef530
aneni <aneni.kidanu@aastustudent.edu.et>
2025-08-24 14:07:29 -0800

    add script for task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+-- task_4.sql
+-- Print the full description of the books table
+-- from the database passed as an argument
+
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'books';
